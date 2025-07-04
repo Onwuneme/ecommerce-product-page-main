255a5bd6e9e7cc8f13eef902de56dcd28b30d923
Onwuneme <146716349+Onwuneme@users.noreply.github.com>
2025-07-04 14:40:25 +0100

    work done

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
 import { createContext, useState } from "react";
+import img1 from "../public/image-product-1.jpg";
+import img2 from "../public/image-product-2.jpg";
+import img3 from "../public/image-product-3.jpg";
+import img4 from "../public/image-product-4.jpg";
+import thumbnail1 from "../public/image-product-1-thumbnail.jpg";
+import thumbnail2 from "../public/image-product-2-thumbnail.jpg";
+import thumbnail3 from "../public/image-product-3-thumbnail.jpg";
+import thumbnail4 from "../public/image-product-4-thumbnail.jpg";
 import "./App.css";
 import Nav from "./Component/Nav";
 import Product from "./Component/Product";
 export const Context = createContext();
 const items = {
   name: "Fall Limited Edition Sneakers",
-  image: [
-    "./src/assets/image-product-1.jpg",
-    "./src/assets/image-product-2.jpg",
-    "./src/assets/image-product-3.jpg",
-    "./src/assets/image-product-4.jpg",
-  ],
-  thumbnail: [
-    "./src/assets/image-product-1-thumbnail.jpg",
-    "./src/assets/image-product-2-thumbnail.jpg",
-    "./src/assets/image-product-3-thumbnail.jpg",
-    "./src/assets/image-product-4-thumbnail.jpg",
-  ],
+  image: [img1, img2, img3, img4],
+  thumbnail: [thumbnail1, thumbnail2, thumbnail3, thumbnail4],
   price: 250.0,
 };
 
